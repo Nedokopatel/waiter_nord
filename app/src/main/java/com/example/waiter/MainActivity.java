8d4d1b958d2028fbfaaad9eaeb32bb598cd5b6ef
Kirill Afrikanov <zeitpanzer@mail.ru>
2021-08-18 07:06:35 +0300

    add upgrade

--- app/src/main/java/com/example/waiter/MainActivity.java
+++ app/src/main/java/com/example/waiter/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.waiter;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Вход");
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        Button entry = (Button) findViewById(R.id.entry);
+
+        entry.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), GreetingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
